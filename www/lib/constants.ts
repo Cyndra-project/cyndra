465935d6375473c4b54af6c79ea06ac40e10587d
Cyndra-project <junaidgohan123@gmail.com>
2024-07-13 23:30:30 +0400

    updated home page (#102)

--- www/lib/constants.ts
+++ www/lib/constants.ts
@@ -1,13 +1,18 @@
-export const APP_NAME = 'cyndra'
+export const APP_NAME = "cyndra";
 
-export const SITE_TITLE = 'Deploy Rust apps with a single Cargo command'
+export const SITE_TITLE = "Deploy Rust apps with a single Cargo command";
 
-export const SITE_DESCRIPTION = 'Cyndra is a crate that uses traits and annotations to configure your backend deployment - including databases.'
+export const SITE_DESCRIPTION =
+  "Cyndra is a crate that uses traits and annotations to configure your backend deployment - including databases.";
 
-export const SITE_URL = 'https://cyndra.rs/'
+export const SITE_URL = "https://cyndra.rs/";
 
-export const TWITTER_HANDLE = '@getsynth'
+export const TWITTER_HANDLE = "@cyndra_dev";
 
-export const MIXPANEL_TOKEN = 'ea13713300ea92e06107f27762512e57'
+export const MIXPANEL_TOKEN = "ea13713300ea92e06107f27762512e57";
 
-export const DISCORD_URL = 'https://discord.gg/H33rRDTm3p'
+export const GITHUB_URL = "https://github.com/getsynth/cyndra";
+export const DISCORD_URL = "https://discord.gg/H33rRDTm3p";
+export const TWITTER_URL = "https://twitter.com/cyndra_dev";
+export const cyndra_DOCS_URL =
+  "https://docs.rs/cyndra-service/latest/cyndra_service/";
