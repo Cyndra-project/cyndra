cdc7bb3c9fd96dc37df074192fa183816814272a
Cyndra-project <junaidgohan123@gmail.com>
2024-07-18 03:50:16 +0200

    feat: load initial user from env

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -47,4 +47,17 @@ export PROXY_PORT=${PROXY_PORT:-8000}
 
 export API_PORT=${API_PORT:-8001}
 
+if [[ ! -z "${cyndra_USERS_TOML}" && ! -s "${cyndra_USERS_TOML}" ]]
+then
+    if [[ -z "${cyndra_INITIAL_KEY}" ]]
+    then
+        echo "\$cyndra_INITIAL_KEY is not set to create initial user's key"
+        exit 1
+    fi
+
+    echo "Creating a first user with key '${cyndra_INITIAL_KEY}' at '${cyndra_USERS_TOML}'"
+    mkdir -p $(dirname "${cyndra_USERS_TOML}")
+    echo -e "[$cyndra_INITIAL_KEY]\nname = 'first-user'\nprojects = []" > "${cyndra_USERS_TOML}"
+fi
+
 exec supervisord -n -c /usr/share/supervisord/supervisord.conf
