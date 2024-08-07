dd8e98bd45f92e4f480a0b1df58d074f427f8450
Cyndra-project <junaidgohan123@gmail.com>
2024-08-07 03:57:33 +0100

    feat: run a service's unit tests on server side before loading and executing (#227)
    * feat: run a service's tests on server side before deployment
    
    * feat: run tests based on URL parameter
    
    * fix: remove unnecessary println
    
    * refactor: use contains_key instead of get(...).is_none
    
    * test: pre-deployment test success and failure

---
+++ deployer/tests/resources/tests-pass/src/lib.rs
@@ -0,0 +1,21 @@
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[cyndra_service::main]
+async fn rocket() -> cyndra_service::CyndraRocket {
+    let rocket = rocket::build().mount("/hello", routes![index]);
+    Ok(rocket)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn this_fails() {
+        assert_eq!(super::index(), "Hello, world!");
+    }
+}
