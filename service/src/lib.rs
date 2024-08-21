35f9095a9e5d86e12e4c58a5cad722b1d7fe5457
Cyndra-project <junaidgohan123@gmail.com>
2024-08-21 20:52:51 +0800

    docs: fix typos (#278)

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -12,7 +12,7 @@
 //! Cyndra is an open-source app platform that uses traits and annotations to configure your backend deployments.
 //!
 //! ## Usage
-//! Start by installing the [`cargo cyndra`](https://docs.rs/crate/cargo-cyndra/latest) subcommand by runnning the following in a terminal:
+//! Start by installing the [`cargo cyndra`](https://docs.rs/crate/cargo-cyndra/latest) subcommand by running the following in a terminal:
 //!
 //! ```bash
 //! $ cargo install cargo-cyndra
@@ -286,9 +286,10 @@ extern crate cyndra_codegen;
 /// ## cyndra managed dependencies
 /// The following dependencies can be managed by cyndra - remember to enable their feature flags for the `cyndra-service` dependency in `Cargo.toml` and configure them using an attribute annotation:
 ///
+
 /// | Argument type                                                            | Feature flag        | Attribute            | Dependency                                                                                         | Example                                                                          |
 /// | ------------------------------------------------------------------------ | ------------------- | -------------------- | -------------------------------------------------------------------------------------------------- | -------------------------------------------------------------------------------- |
-/// | [`PgPool`](https://docs.rs/sqlx/latest/sqlx/type.PgPool.html)            | sqlx-postgres       | `shared::Postgres`   | A shared PostgresSql instance accessed using [sqlx](https://docs.rs/sqlx)                          | [GitHub](https://github.com/getsynth/cyndra/tree/main/examples/rocket/postgres) |
+/// | [`PgPool`](https://docs.rs/sqlx/latest/sqlx/type.PgPool.html)            | sqlx-postgres       | `shared::Postgres`   | A shared PostgresSQL instance accessed using [sqlx](https://docs.rs/sqlx)                          | [GitHub](https://github.com/getsynth/cyndra/tree/main/examples/rocket/postgres) |
 /// | [`Database`](https://docs.rs/mongodb/latest/mongodb/struct.Database.html)| mongodb-integration | `shared::MongoDb`    | A shared MongoDb database accessed using the [mongodb](https://docs.rs/mongodb) driver             | [GitHub](https://github.com/getsynth/cyndra/tree/main/examples/poem/mongodb)    |
 /// | [`MySqlPool`](https://docs.rs/sqlx/latest/sqlx/type.MySqlPool.html)      | sqlx-aws-mariadb    | `aws::rds::MariaDB`  | An AWS RDS MariaDB instance tied to your instance and accessed using [sqlx](https://docs.rs/sqlx)  |                                                                                  |
 /// | [`MySqlPool`](https://docs.rs/sqlx/latest/sqlx/type.MySqlPool.html)      | sqlx-aws-mysql      | `aws::rds::MySql`    | An AWS RDS MySql instance tied to your instance and accessed using [sqlx](https://docs.rs/sqlx)    |                                                                                  |
