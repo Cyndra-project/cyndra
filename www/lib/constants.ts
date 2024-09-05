b58b0546fb7ae0669cb96d8b6ab1f14d29f4160b
Cyndra-project <junaidgohan123@gmail.com>
2024-09-05 19:40:16 +0200

    Feat(www): cyndra beta signup (#421)
    * feat(www): add beta signup form
    
    * feat: add formspree endpoint
    
    * feat: remove socials footer from beta page
    
    * feat: add dummy text above sign up form
    
    * feat: placeholder gif
    
    * feat: align dummy text left
    
    * feat: remove gif, add text
    
    * feat: update beta page text
    
    * feat: update the rest of the site

--- www/lib/constants.ts
+++ www/lib/constants.ts
@@ -1,18 +1,20 @@
 export const APP_NAME = "cyndra";
 
-export const SITE_TITLE = "Stateful Serverless for Rust";
+export const SITE_TITLE = "Build Backends. Fast.";
 
 export const SITE_DESCRIPTION =
-  "Cyndra is a web application platform that uses traits and annotations to configure your backend deployment - including databases.";
+  "Stop worrying about the infrastructure. Focus on writing code, cyndra will do the rest.";
 
 export const SITE_URL = "https://cyndra.rs/";
 
 export const TWITTER_HANDLE = "@cyndra_dev";
 
 export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
+export const FORMSPREE_ENDPOINT = process.env.NEXT_PUBLIC_FORMSPREE_ENDPOINT;
 
 export const GITHUB_URL = "https://github.com/cyndra-hq/cyndra";
 export const DISCORD_URL = "https://discord.gg/cyndra";
 export const TWITTER_URL = "https://twitter.com/cyndra_dev";
-export const cyndra_DOCS_URL =
-  "https://docs.cyndra.rs/";
+export const cyndra_DOCS_URL = "https://docs.cyndra.rs/";
+export const CONTRIBUTING_URL =
+  "https://github.com/cyndra-hq/cyndra/blob/main/CONTRIBUTING.md";
