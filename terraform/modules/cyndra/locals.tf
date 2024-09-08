1c8d46677a6dc309441dadc8bfffa8e05fce5c88
Cyndra-project <junaidgohan123@gmail.com>
2024-09-08 22:38:13 +0000

    refactor: tf files have been moved to cyndra-hq/terraform-aws-cyndra

--- terraform/modules/cyndra/locals.tf
+++
@@ -1,8 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-locals {
-  account_id               = data.aws_caller_identity.current.account_id
-  data_dir                 = "/opt/cyndra"
-  docker_backend_image     = "public.ecr.aws/cyndra/api"
-  docker_provisioner_image = "public.ecr.aws/cyndra/provisioner"
-}
