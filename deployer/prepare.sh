cb97bb33799c1ffda16956a6f13d5608301590ea
Cyndra-project <junaidgohan123@gmail.com>
2024-10-14 11:46:28 +0200

    Merge remote-tracking branch 'upstream/main' into cyndra-next

--- deployer/prepare.sh
+++ deployer/prepare.sh
@@ -14,19 +14,27 @@ cyndra-persist = { path = "/usr/src/cyndra/resources/persist" }
 cyndra-shared-db = { path = "/usr/src/cyndra/resources/shared-db" }
 cyndra-secrets = { path = "/usr/src/cyndra/resources/secrets" }
 cyndra-static-folder = { path = "/usr/src/cyndra/resources/static-folder" }' > $CARGO_HOME/config.toml
-
+#
 # Add the wasm32-wasi target
 rustup target add wasm32-wasi
 
 # Install the cyndra runtime
-cargo install cyndra-runtime --path "/usr/src/cyndra/runtime" 
+cargo install cyndra-runtime --path "/usr/src/cyndra/runtime"
 
-# Make future crates requests to our own mirror
-echo '
+while getopts "p," o; do
+    case $o in
+        "p")
+            # Make future crates requests to our own mirror
+            echo '
 [source.cyndra-crates-io-mirror]
 registry = "http://panamax:8080/git/crates.io-index"
 [source.crates-io]
 replace-with = "cyndra-crates-io-mirror"' >> $CARGO_HOME/config.toml
+            ;;
+        *)
+            ;;
+    esac
+done
 
 # Prefetch crates.io index from our mirror
 # TODO: restore when we know how to prefetch from our mirror
--- resources/README.md
+++ resources/README.md
@@ -1,5 +1,11 @@
+# Resources
+
 ## Managed resources
-The list of managed resources for cyndra is always growing. If you feel we are missing a resource you would like, then feel to create a feature request for your desired resource.
+
+The list of managed resources for cyndra is always growing.
+If you feel we are missing a resource you would like, then feel to create a feature request for your desired resource.
 
 ## Writing your own managed resources
-Creating your own resources is actually easy. You only need to implement the [`ResourceBuilder<T>`](https://docs.rs/cyndra-service/latest/cyndra_service/trait.ResourceBuilder.html) trait for your resource.
+
+Creating your own resources is actually easy.
+You only need to implement the [`ResourceBuilder<T>`](https://docs.rs/cyndra-service/latest/cyndra_service/trait.ResourceBuilder.html) trait for your resource.
