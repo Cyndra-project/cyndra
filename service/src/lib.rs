70580fc4d0666a3856e5b029c40abb88345bf392
Cyndra-project <junaidgohan123@gmail.com>
2024-10-14 20:20:03 +0200

    misc: v0.12.0-rc1 (#708)
    * misc: v0.12.0-rc1
    
    * misc: publish proto
    
    * refactor: remove relative from build.rs
    
    * refactor: add description to proto
    
    * refactor: make runtime publishable
    
    * refactor: make actix publishable
    
    * feat: make cyndra-axum publishable
    
    * feat: make poem publishable
    
    * feat: make poise publishable
    
    * feat: make cyndra-rocket publishable
    
    * refactor: make warp publishable
    
    * refactor: make tower publishable
    
    * refactor: make tide publishable
    
    * refactor: make thruster publishable
    
    * refactor: make serenity publishable
    
    * refactor: make salvo publishable
    
    ---------

--- service/src/lib.rs
+++ service/src/lib.rs
@@ -27,7 +27,7 @@
 //! be a library crate with a `cyndra-service` dependency with the `web-rocket` feature on the `cyndra-service` dependency.
 //!
 //! ```toml
-//! cyndra-service = { version = "0.11.0", features = ["web-rocket"] }
+//! cyndra-service = { version = "0.12.0-rc1", features = ["web-rocket"] }
 //! ```
 //!
 //! A boilerplate code for your rocket project can also be found in `src/lib.rs`:
@@ -107,7 +107,7 @@
 //! Add `cyndra-shared-db` as a dependency with the `postgres` feature, and add `sqlx` as a dependency with the `runtime-tokio-native-tls` and `postgres` features inside `Cargo.toml`:
 //!
 //! ```toml
-//! cyndra-shared-db = { version = "0.11.0", features = ["postgres"] }
+//! cyndra-shared-db = { version = "0.12.0-rc1", features = ["postgres"] }
 //! sqlx = { version = "0.6.2", features = ["runtime-tokio-native-tls", "postgres"] }
 //! ```
 //!
