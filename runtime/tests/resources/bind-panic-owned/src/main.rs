2b2077bb1fa7b16e971b2c27e16558042980ab2d
Cyndra-project <junaidgohan123@gmail.com>
2024-10-28 15:17:19 -0400

    fix: some panic messages get lost (#854)
    * fix: some panic messages get lost
    
    * feat: tests for loader and bind panics
    
    ---------

---
+++ runtime/tests/resources/bind-panic-owned/src/main.rs
@@ -0,0 +1,13 @@
+struct MyService;
+
+#[cyndra_runtime::async_trait]
+impl cyndra_runtime::Service for MyService {
+    async fn bind(mut self, _: std::net::SocketAddr) -> Result<(), cyndra_runtime::Error> {
+        panic!("panic in {}", "bind");
+    }
+}
+
+#[cyndra_runtime::main]
+async fn bind_panic_owned() -> Result<MyService, cyndra_runtime::Error> {
+    Ok(MyService)
+}
