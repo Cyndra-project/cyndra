afa009c3a275fa3d31cc57490aa7b68b69224aa1
Cyndra-project <junaidgohan123@gmail.com>
2024-10-31 08:16:32 +0200

    feat: allow resetting a user's API-key (#857)
    * feat: allow resetting a users API-key
    
    A user should be able to reset their api-key, this is important functionality to have in case users leak their key. This should generate a new api-key and persist it in the users table.
    
    Fixes #838
    
    * clippy
    
    * chore: some cleanup, code style, etc.
    
    * account name from cookie or key
    
    * invalidate auth_layer cache
    
    * fixes
    
    * Add `logout --invalidate-api-key`
    
    * Rename to reset, add success msg and login url hint.
    
    * fmt
    
    ---------

--- cargo-cyndra/src/lib.rs
+++ cargo-cyndra/src/lib.rs
@@ -4,6 +4,7 @@ pub mod config;
 mod init;
 mod provisioner_server;
 
+use args::LogoutArgs;
 use indicatif::ProgressBar;
 use cyndra_common::claims::{ClaimService, InjectPropagation};
 use cyndra_common::models::deployment::get_deployments_table;
@@ -29,7 +30,7 @@ use std::path::{Path, PathBuf};
 use std::process::exit;
 use std::str::FromStr;
 
-use anyhow::{bail, Context, Result};
+use anyhow::{anyhow, bail, Context, Result};
 pub use args::{Args, Command, DeployArgs, InitArgs, LoginArgs, ProjectArgs, RunArgs};
 use cargo_metadata::Message;
 use clap::CommandFactory;
@@ -57,6 +58,8 @@ use crate::provisioner_server::LocalProvisioner;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const MANIFEST_DIR: &str = env!("CARGO_MANIFEST_DIR");
+const cyndra_LOGIN_URL: &str = "https://cyndra.rs/login";
+const cyndra_GH_ISSUE_URL: &str = "https://github.com/cyndra-hq/cyndra/issues/new";
 
 pub struct Cyndra {
     ctx: RequestContext,
@@ -70,6 +73,8 @@ impl Cyndra {
 
     pub async fn run(mut self, mut args: Args) -> Result<CommandOutcome> {
         trace!("running local client");
+
+        // All commands that need to know which project is being handled
         if matches!(
             args.cmd,
             Command::Deploy(..)
@@ -98,11 +103,11 @@ impl Cyndra {
             Command::Init(init_args) => self.init(init_args, args.project_args).await,
             Command::Generate { shell, output } => self.complete(shell, output).await,
             Command::Login(login_args) => self.login(login_args).await,
-            Command::Logout => self.logout().await,
+            Command::Logout(logout_args) => self.logout(logout_args).await,
             Command::Feedback => self.feedback().await,
             Command::Run(run_args) => self.local_run(run_args).await,
             Command::Deploy(deploy_args) => {
-                return self.deploy(deploy_args, &self.client()?).await;
+                return self.deploy(&self.client()?, deploy_args).await;
             }
             Command::Status => self.status(&self.client()?).await,
             Command::Logs { id, latest, follow } => {
@@ -249,10 +254,9 @@ impl Cyndra {
 
     /// Provide feedback on GitHub.
     async fn feedback(&self) -> Result<()> {
-        let url = "https://github.com/cyndra-hq/cyndra/issues/new";
-        let _ = webbrowser::open(url);
+        let _ = webbrowser::open(cyndra_GH_ISSUE_URL);
+        println!("If your browser did not open automatically, go to {cyndra_GH_ISSUE_URL}");
 
-        println!("\nIf your browser did not open automatically, go to {url}");
         Ok(())
     }
 
@@ -261,10 +265,8 @@ impl Cyndra {
         let api_key_str = match login_args.api_key {
             Some(api_key) => api_key,
             None => {
-                let url = "https://cyndra.rs/login";
-                let _ = webbrowser::open(url);
-
-                println!("If your browser did not automatically open, go to {url}");
+                let _ = webbrowser::open(cyndra_LOGIN_URL);
+                println!("If your browser did not automatically open, go to {cyndra_LOGIN_URL}");
 
                 Password::with_theme(&ColorfulTheme::default())
                     .with_prompt("API key")
@@ -280,11 +282,26 @@ impl Cyndra {
         Ok(())
     }
 
-    async fn logout(&mut self) -> Result<()> {
+    async fn logout(&mut self, logout_args: LogoutArgs) -> Result<()> {
+        if logout_args.reset_api_key {
+            self.reset_api_key(&self.client()?).await?;
+            println!("Successfully reset the API key.");
+            println!(" -> Go to {cyndra_LOGIN_URL} to get a new one.\n");
+        }
         self.ctx.clear_api_key()?;
-
         println!("Successfully logged out of cyndra.");
+
         Ok(())
+    }
+
+    async fn reset_api_key(&self, client: &Client) -> Result<()> {
+        client.reset_api_key().await.and_then(|res| {
+            if res.status().is_success() {
+                Ok(())
+            } else {
+                Err(anyhow!("Resetting API key failed."))
+            }
+        })
     }
 
     async fn stop(&self, client: &Client) -> Result<()> {
@@ -867,7 +884,7 @@ impl Cyndra {
         Ok(())
     }
 
-    async fn deploy(&self, args: DeployArgs, client: &Client) -> Result<CommandOutcome> {
+    async fn deploy(&self, client: &Client, args: DeployArgs) -> Result<CommandOutcome> {
         if !args.allow_dirty {
             self.is_dirty()?;
         }
