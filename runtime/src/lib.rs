6602af59a898491ea0f8653bee7704cd3986c641
Cyndra-project <junaidgohan123@gmail.com>
2024-11-11 04:53:11 +0200

    chore: move codegen::main from service to runtime (#1013)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -20,7 +20,7 @@
 //!
 //! Now that cyndra is installed, you can initialize a project with Axum boilerplate:
 //! ```bash
-//! $ cargo cyndra init --axum my-axum-app
+//! $ cargo cyndra init --template axum my-axum-app
 //! ```
 //!
 //! By looking at the `Cargo.toml` file of the generated `my-axum-app` project you will see it has been made to
@@ -106,7 +106,7 @@
 //!
 //! Initialize a project with Rocket boilerplate:
 //! ```bash
-//! $ cargo cyndra init --rocket my-rocket-app
+//! $ cargo cyndra init --template rocket my-rocket-app
 //! ```
 //!
 //! Add `cyndra-shared-db` as a dependency with the `postgres` feature, and add `sqlx` as a dependency with the
@@ -214,6 +214,59 @@
 //!
 //! You can also [open an issue or a discussion on GitHub](https://github.com/cyndra-hq/cyndra).
 //!
+
+/// Helper macro that generates the entrypoint required by any service - likely the only macro you need in this crate.
+///
+/// # Without cyndra managed resources
+/// The simplest usage is when your service does not require any cyndra managed resources, so you only need to return a cyndra supported service:
+///
+/// ```rust,no_run
+/// use cyndra_rocket::CyndraRocket;
+///
+/// #[cyndra_rocket::main]
+/// async fn rocket() -> CyndraRocket {
+///     let rocket = rocket::build();
+///
+///     Ok(rocket.into())
+/// }
+/// ```
+///
+/// ## cyndra supported services
+/// The following types can be returned from a `#[cyndra_service::main]` function and enjoy first class service support in cyndra.
+///
+/// | Return type                           | Crate                                                         | Service                                     | Version    | Example                                                                               |
+/// | ------------------------------------- |-------------------------------------------------------------- | ------------------------------------------- | ---------- | -----------------------------------------------------------------------------------   |
+/// | `CyndraActixWeb`                     |[cyndra-actix-web](https://crates.io/crates/cyndra-actix-web)| [actix-web](https://docs.rs/actix-web/4.3)  | 4.3        | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/actix-web/hello-world)      |
+/// | `CyndraAxum`                         |[cyndra-axum](https://crates.io/crates/cyndra-axum)          | [axum](https://docs.rs/axum/0.6)            | 0.5        | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/axum/hello-world)           |
+/// | `CyndraPoem`                         |[cyndra-poem](https://crates.io/crates/cyndra-poem)          | [poem](https://docs.rs/poem/1.3)            | 1.3        | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/poem/hello-world)           |
+/// | `CyndraPoise`                        |[cyndra-poise](https://crates.io/crates/cyndra-poise)        | [poise](https://docs.rs/poise/0.5)          | 0.5        | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/poise/hello-world)          |
+/// | `CyndraRocket`                       |[cyndra-rocket](https://crates.io/crates/cyndra-rocket)      | [rocket](https://docs.rs/rocket/0.5.0-rc.2) | 0.5.0-rc.2 | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/rocket/hello-world)         |
+/// | `CyndraSalvo`                        |[cyndra-salvo](https://crates.io/crates/cyndra-salvo)        | [salvo](https://docs.rs/salvo/0.37)         | 0.37       | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/salvo/hello-world)          |
+/// | `CyndraSerenity`                     |[cyndra-serenity](https://crates.io/crates/cyndra-serenity   | [serenity](https://docs.rs/serenity/0.11)   | 0.11       | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/serenity/hello-world)       |
+/// | `CyndraThruster`                     |[cyndra-thruster](https://crates.io/crates/cyndra-thruster)  | [thruster](https://docs.rs/thruster/1.3)    | 1.3        | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/thruster/hello-world)       |
+/// | `CyndraTower`                        |[cyndra-tower](https://crates.io/crates/cyndra-tower)        | [tower](https://docs.rs/tower/0.4)          | 0.4        | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/tower/hello-world)          |
+/// | `CyndraTide`                         |[cyndra-tide](https://crates.io/crates/cyndra-tide)          | [tide](https://docs.rs/tide/0.16)           | 0.16       | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/tide/hello-world)           |
+///
+/// # Getting cyndra managed resources
+/// Cyndra is able to manage resource dependencies for you. These resources are passed in as inputs to your `#[cyndra_runtime::main]` function and are configured using attributes:
+/// ```rust,no_run
+/// use sqlx::PgPool;
+/// use cyndra_rocket::CyndraRocket;
+///
+/// struct MyState(PgPool);
+///
+/// #[cyndra_runtime::main]
+/// async fn rocket(#[cyndra_shared_db::Postgres] pool: PgPool) -> CyndraRocket {
+///     let state = MyState(pool);
+///     let rocket = rocket::build().manage(state);
+///
+///     Ok(rocket.into())
+/// }
+/// ```
+///
+/// More [cyndra managed resources can be found here](https://github.com/cyndra-hq/cyndra/tree/main/resources)
+pub use cyndra_codegen::main;
+
 mod alpha;
 mod args;
 mod logger;
@@ -223,14 +276,15 @@ mod provisioner_factory;
 mod resource_tracker;
 
 pub use alpha::{start, Alpha};
-pub use async_trait::async_trait;
 pub use logger::Logger;
 #[cfg(feature = "next")]
 pub use next::{AxumWasm, NextArgs};
 pub use provisioner_factory::ProvisionerFactory;
 pub use resource_tracker::{get_resource, ResourceTracker};
 pub use cyndra_common::storage_manager::StorageManager;
-pub use cyndra_service::{main, CustomError, Error, Factory, ResourceBuilder, Service};
+pub use cyndra_service::{CustomError, Error, Factory, ResourceBuilder, Service};
+
+pub use async_trait::async_trait;
 
 // Dependencies required by the codegen
 pub use anyhow::Context;
