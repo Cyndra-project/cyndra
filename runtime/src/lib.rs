afb3e6c2006f5ff96d702787cae3275eef25f459
Cyndra-project <junaidgohan123@gmail.com>
2024-11-21 11:49:36 +0200

    Chore/bump sqlx (#1118)
    * chore: bump sqlx
    
    * refactor: implement breaking sqlx changes
    
    * fix: missing tokio feature in deployer

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -114,7 +114,7 @@
 //!
 //! ```toml
 //! cyndra-shared-db = { version = "0.21.0", features = ["postgres"] }
-//! sqlx = { version = "0.6.2", features = ["runtime-tokio-native-tls", "postgres"] }
+//! sqlx = { version = "0.7.1", features = ["runtime-tokio-native-tls", "postgres"] }
 //! ```
 //!
 //! Now update the `#[cyndra_runtime::main]` function to take in a `PgPool`:
