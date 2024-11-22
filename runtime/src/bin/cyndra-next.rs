121ec392ce6d198be06708148cb3d6a205aabe9c
Cyndra-project <junaidgohan123@gmail.com>
2024-11-22 20:49:01 +0200

    feat(runtime): add alpha runtime version check (#1088)
    * feat(runtime): add alpha runtime version check
    
    Add a version flag check to the alpha runtime and
    check if its version is the same as the version
    of cargo-cyndra. Warn the user if that is not the
    case and give hints. The next runtime already has
    such a check.
    
    Closes #1053
    
    * fix: apply lint from cargo clippy
    
    * fix: check version compatibility and improve hints
    
    Use cargo's rule for checking if two versions are
    compatible. Give hints on how to update the older
    componenent.
    
    * fix: share `print_version` between both runtimes
    
    Use the same function in 'alpha' and 'next' for
    printing the current version of cyndra-runtime.
    
    * fix: make `print_version` pub
    
    Allow use of `print_version` in next binary by
    making `print_version` available to it, too.
    
    * fix: remove duplication and apply lints
    
    * Update cargo-cyndra/src/lib.rs
    
    
    ---------

--- runtime/src/bin/cyndra-next.rs
+++ runtime/src/bin/cyndra-next.rs
@@ -5,7 +5,7 @@ use std::{
 
 use cyndra_common::backends::tracing::{setup_tracing, ExtractPropagationLayer};
 use cyndra_proto::runtime::runtime_server::RuntimeServer;
-use cyndra_runtime::{AxumWasm, NextArgs};
+use cyndra_runtime::{print_version, AxumWasm, NextArgs};
 use tonic::transport::Server;
 use tracing::trace;
 
@@ -35,12 +35,4 @@ async fn main() {
     let router = server_builder.add_service(svc);
 
     router.serve(addr).await.unwrap();
-}
-
-fn print_version() {
-    // same way `clap` gets these
-    let name = env!("CARGO_PKG_NAME");
-    let version = env!("CARGO_PKG_VERSION");
-
-    println!("{name} {version}");
 }
