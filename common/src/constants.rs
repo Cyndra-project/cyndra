5848804899141d332eecc9993d325567dd8c7e3f
Cyndra-project <junaidgohan123@gmail.com>
2024-12-14 07:12:07 +0200

    feat: project delete (#1307)
    * feat(cargo-cyndra): project delete command
    
    * fix(cargo-cyndra): Refactor spinner, fix spam on service stop command
    
    * feat(gateway): delete container and volume, delete from db, bump bollard, layout for gateway endpoint
    
    * feat(gateway): add checks before project delete. fix stopping of running deployment.
    
    * feat(gateway): use status endpoint, check for 404's, delete custom domains, fix stop command (caveat in testcase)
    
    * fix(provisioner): validate project name
    
    * fix(provisioner): gracefully handle deletion of non-existent db
    
    * feat(gateway): project delete sqlx transaction
    
    * test(gateway service): add basic project delete tests
    
    * feat(gateway): block project delete on db or secrets (#1338)
    
    ---------

--- common/src/constants.rs
+++ common/src/constants.rs
@@ -6,10 +6,12 @@ pub const EXECUTABLE_DIRNAME: &str = ".cyndra-executables";
 /// Where general files will persist across deploys, relative to workspace root. Used by plugins.
 pub const STORAGE_DIRNAME: &str = ".cyndra-storage";
 
+pub const API_URL_LOCAL: &str = "http://localhost:8001";
+pub const API_URL_PRODUCTION: &str = "https://api.cyndra.rs";
 #[cfg(debug_assertions)]
-pub const API_URL_DEFAULT: &str = "http://localhost:8001";
+pub const API_URL_DEFAULT: &str = API_URL_LOCAL;
 #[cfg(not(debug_assertions))]
-pub const API_URL_DEFAULT: &str = "https://api.cyndra.rs";
+pub const API_URL_DEFAULT: &str = API_URL_PRODUCTION;
 
 // Crate names for checking cargo metadata
 pub const NEXT_NAME: &str = "cyndra-next";
