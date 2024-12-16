4e43c22f3c0e16337eedd79ca91d4d1e86ca7bc0
Cyndra-project <junaidgohan123@gmail.com>
2024-12-16 21:50:01 +0100

    feat(gateway): enforce project limits on project creation (#1331)
    ---------

--- common/src/constants.rs
+++ common/src/constants.rs
@@ -16,3 +16,8 @@ pub const API_URL_DEFAULT: &str = API_URL_PRODUCTION;
 // Crate names for checking cargo metadata
 pub const NEXT_NAME: &str = "cyndra-next";
 pub const RUNTIME_NAME: &str = "cyndra-runtime";
+
+pub mod limits {
+    pub const MAX_PROJECTS_DEFAULT: u32 = 3;
+    pub const MAX_PROJECTS_EXTRA: u32 = 15;
+}
