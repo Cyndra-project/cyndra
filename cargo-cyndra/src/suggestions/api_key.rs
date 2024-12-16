a31abb655e92dea9bba66cde5089184698cd038d
Cyndra-project <junaidgohan123@gmail.com>
2024-12-16 17:55:50 +0100

    fix: typos found in codebase (#1360)
    * fix typos found in codebase
    
    * fix compiler issue
    
    * unfix: typo

--- cargo-cyndra/src/suggestions/api_key.rs
+++ cargo-cyndra/src/suggestions/api_key.rs
@@ -14,6 +14,6 @@ pub fn reset_api_key_failed(err: anyhow::Error) -> anyhow::Error {
     println!();
     println!("{}", "Logging out failed".red());
     println!();
-    println!("If trying to log out and reset the API key at the same time fails repeteadly, please check Cyndra status at https://status.cyndra.rs or open a help thread on the Discord server.");
+    println!("If trying to log out and reset the API key at the same time fails repeatedly, please check Cyndra status at https://status.cyndra.rs or open a help thread on the Discord server.");
     err
 }
