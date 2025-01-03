f687891adccbc2c7c2b54a84e0bdfab904ac0f19
Cyndra-project <junaidgohan123@gmail.com>
2025-01-03 09:48:22 +0400

    fix(deployer): return empty list when when no service is found (#1495)

--- cargo-cyndra/src/lib.rs
+++ cargo-cyndra/src/lib.rs
@@ -849,7 +849,12 @@ impl Cyndra {
         let table = get_deployments_table(&deployments, proj_name, page, raw, page_hint);
 
         println!("{table}");
-        println!("Run `cargo cyndra logs <id>` to get logs for a given deployment.");
+
+        if deployments.is_empty() {
+            println!("Run `cargo cyndra deploy` to deploy your project.");
+        } else {
+            println!("Run `cargo cyndra logs <id>` to get logs for a given deployment.");
+        }
 
         Ok(CommandOutcome::Ok)
     }
