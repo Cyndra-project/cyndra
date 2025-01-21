281e97bd78499a564c446e08710f99c3ded4b8a7
Cyndra-project <junaidgohan123@gmail.com>
2025-01-21 12:31:28 +0900

    Improvement: add `--raw` flag to `run`, `deploy` command (#1653)
    * add: LogsArgs struct and Logformat enum
    modify: logs() argument
    modify: output log
    
    * add: --raw flag to deploy, run
    
    * fix: common-tests
    
    * fix: tiny
    
    * fix: logs argument

--- common-tests/src/cargo_cyndra.rs
+++ common-tests/src/cargo_cyndra.rs
@@ -30,6 +30,7 @@ pub async fn cargo_cyndra_run(working_directory: &str, external: bool) -> String
         port,
         external,
         release: false,
+        raw: false,
         secret_args: Default::default(),
     };
 
