761ef6f91454e30e6c0256aebd162d7f7a8448bf
Cyndra-project <junaidgohan123@gmail.com>
2025-01-27 22:01:45 +0100

    feat: orgs (#1720)
    * feat: simple org management
    
    * feat: project to / from org transferal
    
    * feat: add org routes to gw
    
    * refactor: touch ups
    
    * refactor: remove PoC commented implementations
    
    * refactor: clippy suggestions
    
    * refactor: more formating issues
    
    * refactor: add validation
    
    * Apply suggestions from code review
    
    
    ---------

--- gateway/src/lib.rs
+++ gateway/src/lib.rs
@@ -17,6 +17,7 @@ use hyper::client::HttpConnector;
 use hyper::Client;
 use once_cell::sync::Lazy;
 use service::ContainerSettings;
+use cyndra_backends::client::permit;
 use cyndra_backends::project_name::ProjectName;
 use cyndra_common::models::error::{ApiError, ErrorKind};
 use cyndra_common::models::user::UserId;
@@ -106,6 +107,12 @@ impl From<io::Error> for Error {
 
 impl From<AcmeClientError> for Error {
     fn from(error: AcmeClientError) -> Self {
+        Self::source(ErrorKind::Internal, error)
+    }
+}
+
+impl From<permit::Error> for Error {
+    fn from(error: permit::Error) -> Self {
         Self::source(ErrorKind::Internal, error)
     }
 }
