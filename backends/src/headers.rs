3054966ba2227c87f89618e9095d88fdfc829de0
Cyndra-project <junaidgohan123@gmail.com>
2025-01-29 13:40:21 +0200

    feat: add models and header for new ecs provisioner (#1730)

--- backends/src/headers.rs
+++ backends/src/headers.rs
@@ -66,6 +66,38 @@ impl Header for XCyndraProjectSecret {
     }
 }
 
+/// Used to ensure requests originate from the control service.
+pub static X_cyndra_CTL_SECRET: HeaderName = HeaderName::from_static("x-cyndra-ctl-secret");
+
+pub struct XCyndraCtlSecret(pub String);
+
+impl Header for XCyndraCtlSecret {
+    fn name() -> &'static HeaderName {
+        &X_cyndra_CTL_SECRET
+    }
+
+    fn decode<'i, I>(values: &mut I) -> Result<Self, headers::Error>
+    where
+        Self: Sized,
+        I: Iterator<Item = &'i HeaderValue>,
+    {
+        let value = values
+            .next()
+            .ok_or_else(headers::Error::invalid)?
+            .to_str()
+            .map_err(|_| headers::Error::invalid())?
+            .to_string();
+
+        Ok(Self(value))
+    }
+
+    fn encode<E: Extend<HeaderValue>>(&self, values: &mut E) {
+        if let Ok(value) = HeaderValue::from_str(self.0.as_str()) {
+            values.extend(std::iter::once(value));
+        }
+    }
+}
+
 /// Used by deployers <=0.38.0. Can be removed when those are no longer supported
 pub static X_cyndra_PROJECT: HeaderName = HeaderName::from_static("x-cyndra-project");
 
