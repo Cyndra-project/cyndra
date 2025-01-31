58e7aca8c7eff0279c4184d6fc5931ecda4c31d3
Cyndra-project <junaidgohan123@gmail.com>
2025-01-31 17:21:47 +0200

    feat(cargo-cyndra beta): enable resource list (#1744)

--- cargo-cyndra/src/lib.rs
+++ cargo-cyndra/src/lib.rs
@@ -132,7 +132,7 @@ impl Cyndra {
             }
             if matches!(
                 args.cmd,
-                Command::Resource(..)
+                Command::Resource(ResourceCommand::Delete { .. })
                     | Command::Stop
                     | Command::Clean
                     | Command::Status
