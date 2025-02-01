bc0130a16622155f1f8222277f6efc55658d0c76
Cyndra-project <junaidgohan123@gmail.com>
2025-02-01 18:13:35 +0100

    feat(cyndra-turso): change turso resource output type to database  (#1747)

--- resources/turso/src/lib.rs
+++ resources/turso/src/lib.rs
@@ -1,5 +1,5 @@
 use async_trait::async_trait;
-use libsql::{Builder, Connection};
+use libsql::{Builder, Database};
 use serde::{Deserialize, Serialize};
 use cyndra_service::{
     error::{CustomError, Error as CyndraError},
@@ -120,8 +120,8 @@ impl ResourceInputBuilder for Turso {
 }
 
 #[async_trait]
-impl IntoResource<Connection> for TursoOutput {
-    async fn into_resource(self) -> Result<Connection, cyndra_service::Error> {
+impl IntoResource<Database> for TursoOutput {
+    async fn into_resource(self) -> Result<Database, cyndra_service::Error> {
         let database = if self.remote {
             Builder::new_remote(
                 self.conn_url.to_string(),
@@ -136,10 +136,8 @@ impl IntoResource<Connection> for TursoOutput {
         } else {
             Builder::new_local(self.conn_url.to_string()).build().await
         };
-        database
-            .map_err(|err| CyndraError::Custom(err.into()))?
-            .connect()
-            .map_err(|err| CyndraError::Custom(err.into()))
+
+        database.map_err(|err| CyndraError::Custom(err.into()))
     }
 }
 
