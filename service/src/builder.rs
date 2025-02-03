756f65e857d67aef084c38fcd57a4108b99dce79
Cyndra-project <junaidgohan123@gmail.com>
2025-02-03 10:50:39 +0200

    fix(service): improve workspace member discovery and error (#1758)
    * fix(service): improve workspace member discovery and error
    
    * fix: test
    
    * nit: wording

--- service/src/builder.rs
+++ service/src/builder.rs
@@ -101,6 +101,12 @@ pub async fn build_workspace(
 
     let metadata = async_cargo_metadata(manifest_path.as_path()).await?;
     let packages = find_cyndra_packages(&metadata)?;
+    if packages.is_empty() {
+        bail!(
+            "Did not find any packages that Cyndra can run. \
+            Make sure your crate has a binary target that uses `#[cyndra_runtime::main]`."
+        );
+    }
 
     let services = compile(
         packages,
@@ -147,13 +153,16 @@ pub fn find_cyndra_packages(metadata: &Metadata) -> anyhow::Result<Vec<Package>>
     let mut packages = Vec::new();
     for member in metadata.workspace_packages() {
         // skip non-Cyndra-related crates
-        if !member
+        // (must have runtime dependency and not be just a library)
+        let has_runtime_dep = member
             .dependencies
             .iter()
-            .any(|dependency| dependency.name == RUNTIME_NAME)
-        {
+            .any(|dependency| dependency.name == RUNTIME_NAME);
+        let has_binary_target = member.targets.iter().any(|t| t.is_bin());
+        if !(has_runtime_dep && has_binary_target) {
             continue;
         }
+
         let mut cyndra_deps = member
             .dependencies
             .iter()
@@ -162,7 +171,6 @@ pub fn find_cyndra_packages(metadata: &Metadata) -> anyhow::Result<Vec<Package>>
             .collect::<Vec<_>>();
         cyndra_deps.sort();
         info!(name = member.name, deps = ?cyndra_deps, "Found workspace member with cyndra dependencies");
-        ensure_binary(member)?;
         packages.push(member.to_owned());
     }
 
@@ -188,15 +196,6 @@ pub async fn clean_crate(project_path: &Path) -> anyhow::Result<()> {
     }
 
     Ok(())
-}
-
-/// Make sure the project is a binary for alpha projects.
-fn ensure_binary(package: &Package) -> anyhow::Result<()> {
-    if package.targets.iter().any(|target| target.is_bin()) {
-        Ok(())
-    } else {
-        bail!("Your Cyndra package must be a binary.")
-    }
 }
 
 async fn compile(
--- service/tests/integration/build_crate.rs
+++ service/tests/integration/build_crate.rs
@@ -13,7 +13,9 @@ async fn not_cyndra() {
 }
 
 #[tokio::test]
-#[should_panic(expected = "Your Cyndra package must be a binary.")]
+#[should_panic(
+    expected = "Did not find any packages that Cyndra can run. Make sure your crate has a binary target that uses `#[cyndra_runtime::main]`."
+)]
 async fn not_bin() {
     let (tx, _) = tokio::sync::mpsc::channel::<String>(256);
     let project_path = format!("{}/tests/resources/not-bin", env!("CARGO_MANIFEST_DIR"));
