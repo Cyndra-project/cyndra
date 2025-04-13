cf0b03f48d58fba2e425b651085081f539eacfa0
Cyndra-project <junaidgohan123@gmail.com>
2025-04-13 13:35:14 +0100

    feat: make api-client timeout configurable, increase in admin cli (#1950)

--- cargo-cyndra/src/lib.rs
+++ cargo-cyndra/src/lib.rs
@@ -253,6 +253,7 @@ impl Cyndra {
                     )]))
                     .unwrap(),
                 ),
+                None,
             );
             self.client = Some(client);
             if !args.offline && !self.beta {
