1502a38edb5101fd55bb892c03f0ffa401b23bb0
Cyndra-project <junaidgohan123@gmail.com>
2025-04-26 14:37:40 +0100

    feat: team models, team project lists (#1986)
    * feat(common): team models
    
    * nit: don't print auth0 id
    
    * nit: make project delete prompt show project name
    
    * feat: update project new options
    
    * feat: add team member
    
    * account tier: add growth, remove deployer
    
    * fix: codegen docs
    
    * docs/typos
    
    * show feature flags in account output
    
    * show team in proj list and link
    
    * show team in proj stat
    
    * don't show deployment status in proj stat
    
    * clippy

--- codegen/src/lib.rs
+++ codegen/src/lib.rs
@@ -8,7 +8,7 @@ mod cyndra_main;
 /// ```rust,no_run
 /// use cyndra_rocket::CyndraRocket;
 ///
-/// #[cyndra_rocket::main]
+/// #[cyndra_runtime::main]
 /// async fn rocket() -> CyndraRocket {
 ///     let rocket = rocket::build();
 ///
@@ -17,19 +17,19 @@ mod cyndra_main;
 /// ```
 ///
 /// ## Cyndra supported services
-/// The following types can be returned from a `#[cyndra_service::main]` function and enjoy first class service support in cyndra.
+/// The following types can be returned from a `#[cyndra_runtime::main]` function and enjoy first class service support in cyndra.
 ///
-/// | Return type       | Crate                                                          | Service                                                                          | Version    | Example                                                                                 |
-/// | ----------------- | -------------------------------------------------------------- | -------------------------------------------------------------------------------- | ---------- | --------------------------------------------------------------------------------------- |
-/// | `CyndraActixWeb` | [cyndra-actix-web](https://crates.io/crates/cyndra-actix-web)| [actix-web](https://docs.rs/actix-web/4.3)                                       | 4.3        | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/actix-web/hello-world)|
-/// | `CyndraAxum`     | [cyndra-axum](https://crates.io/crates/cyndra-axum)          | [axum](https://docs.rs/axum/0.7)                                                 | 0.7        | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/axum/hello-world)     |
-/// | `CyndraPoem`     | [cyndra-poem](https://crates.io/crates/cyndra-poem)          | [poem](https://docs.rs/poem/2.0)                                                 | 2.0        | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/poem/hello-world)     |
-/// | `CyndraRocket`   | [cyndra-rocket](https://crates.io/crates/cyndra-rocket)      | [rocket](https://docs.rs/rocket/0.5)                                             | 0.5        | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/rocket/hello-world)   |
-/// | `CyndraSalvo`    | [cyndra-salvo](https://crates.io/crates/cyndra-salvo)        | [salvo](https://docs.rs/salvo/0.63)                                              | 0.63       | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/salvo/hello-world)    |
-/// | `CyndraSerenity` | [cyndra-serenity](https://crates.io/crates/cyndra-serenity)  | [serenity](https://docs.rs/serenity/0.12) and [poise](https://docs.rs/poise/0.6) | 0.12       | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/serenity/hello-world) |
-/// | `CyndraThruster` | [cyndra-thruster](https://crates.io/crates/cyndra-thruster)  | [thruster](https://docs.rs/thruster/1.3)                                         | 1.3        | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/thruster/hello-world) |
-/// | `CyndraTower`    | [cyndra-tower](https://crates.io/crates/cyndra-tower)        | [tower](https://docs.rs/tower/0.4)                                               | 0.4        | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/tower/hello-world)    |
-/// | `CyndraTide`     | [cyndra-tide](https://crates.io/crates/cyndra-tide)          | [tide](https://docs.rs/tide/0.16)                                                | 0.16       | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/tide/hello-world)     |
+/// | Return type       | Crate                                                          | Service                                                                 | Example                                                                                 |
+/// | ----------------- | -------------------------------------------------------------- | ----------------------------------------------------------------------- | --------------------------------------------------------------------------------------- |
+/// | `CyndraActixWeb` | [cyndra-actix-web](https://crates.io/crates/cyndra-actix-web)| [actix-web](https://docs.rs/actix-web)                                  | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/actix-web/hello-world)|
+/// | `CyndraAxum`     | [cyndra-axum](https://crates.io/crates/cyndra-axum)          | [axum](https://docs.rs/axum)                                            | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/axum/hello-world)     |
+/// | `CyndraPoem`     | [cyndra-poem](https://crates.io/crates/cyndra-poem)          | [poem](https://docs.rs/poem)                                            | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/poem/hello-world)     |
+/// | `CyndraRocket`   | [cyndra-rocket](https://crates.io/crates/cyndra-rocket)      | [rocket](https://docs.rs/rocket)                                        | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/rocket/hello-world)   |
+/// | `CyndraSalvo`    | [cyndra-salvo](https://crates.io/crates/cyndra-salvo)        | [salvo](https://docs.rs/salvo)                                          | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/salvo/hello-world)    |
+/// | `CyndraSerenity` | [cyndra-serenity](https://crates.io/crates/cyndra-serenity)  | [serenity](https://docs.rs/serenity) and [poise](https://docs.rs/poise) | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/serenity/hello-world) |
+/// | `CyndraThruster` | [cyndra-thruster](https://crates.io/crates/cyndra-thruster)  | [thruster](https://docs.rs/thruster)                                    | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/thruster/hello-world) |
+/// | `CyndraTower`    | [cyndra-tower](https://crates.io/crates/cyndra-tower)        | [tower](https://docs.rs/tower)                                          | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/tower/hello-world)    |
+/// | `CyndraTide`     | [cyndra-tide](https://crates.io/crates/cyndra-tide)          | [tide](https://docs.rs/tide)                                            | [GitHub](https://github.com/cyndra-hq/cyndra-examples/tree/main/tide/hello-world)     |
 ///
 /// ## Getting cyndra managed resources
 /// Cyndra is able to manage resource dependencies for you. These resources are passed in as inputs to your `#[cyndra_runtime::main]` function and are configured using attributes:
