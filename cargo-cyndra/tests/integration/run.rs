5f9380c4aefa6c849dbd3451b2bf2fb562c5a66f
Cyndra-project <junaidgohan123@gmail.com>
2025-04-30 12:57:24 +0530

    feat: impl live reloading for local runs (#1991)
    * feat(cargo-cyndra): add bacon support for running in watch mode
    
    * feat(cargo-cyndra): enhance bacon configuration for cyndra run
    
    * refactor(cargo-cyndra): simplify bacon run and version check
    
    * fix(cargo-cyndra): improve bacon version check and error handling
    
    * refactor(cargo-cyndra): reorder bacon module import
    
    * refactor(cargo-cyndra): improve bacon run configuration and error handling
    
    * refactor(cargo-cyndra): simplify bacon version parsing
    
    * refactor(cargo-cyndra): add platform-specific bacon configurations
    
    * refactor(cargo-cyndra): replace inline config generation with platform-specific TOML files
    
    * refactor(cargo-cyndra): remove trailing spaces in platform-specific TOML files
    
    * refactor(cargo-cyndra): enhance error handling with context for bacon commands
    
    * nit: module location
    
    * nit: text
    
    * nit: logs, better kill command, run in headless mode
    
    ---------

--- cargo-cyndra/tests/integration/run.rs
+++ cargo-cyndra/tests/integration/run.rs
@@ -41,6 +41,7 @@ async fn cyndra_run(working_directory: &str, external: bool) -> String {
                 external,
                 release: false,
                 raw: false,
+                bacon: false,
                 secret_args: Default::default(),
             }),
         },
